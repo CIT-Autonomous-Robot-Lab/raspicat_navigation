77798715a048e63f17d123eac01ba77be8e35292
uhobeike <beike315@icloud.com>
2022-07-09 10:02:26 +0000

    Fix ci

--- raspicat_waypoint_navigation/test/ci_test.sh
+++ raspicat_waypoint_navigation/test/ci_test.sh
@@ -24,10 +24,10 @@ ffmpeg -nostdin -draw_mouse 0 -f x11grab -video_size 1300x1000 -i :44 -codec:v l
 sleep 10
 
 # Execute start operation
-rosservice call /way_nav_start --wait
+rosservice call --wait /way_nav_start  &
 
 # Execute restart operation
-rostopic echo -n 1 /waypoint_stop_function;rosservice call /way_nav_restart --wait
+rostopic echo -n 1 /waypoint_stop_function;rosservice call --wait /way_nav_restart & 
 
 # Check goal
 rostopic echo -n 1 /waypoint_goal_function
