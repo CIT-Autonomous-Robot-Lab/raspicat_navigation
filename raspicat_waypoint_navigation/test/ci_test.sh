962daf8f85b77f4997bb78881b8fe8e66744f40a
uhobeike <beike315@icloud.com>
2022-11-12 02:20:19 +0000

    Test

--- raspicat_waypoint_navigation/test/ci_test.sh
+++ raspicat_waypoint_navigation/test/ci_test.sh
@@ -7,6 +7,7 @@ while true; do sleep 10 | top -n 1 -b | head -n 20; done &
 # Gazebo launch
 roslaunch raspicat_map2gazebo raspicat_tsudanuma_2_19_world.launch \
   x_gazebo:=0.155971128532 y_gazebo:=-0.0254326737864 yaw_gazebo:=0 open_gui:=false &
+sleep 20
 
 # RViz 
 xvfb-run --listen-tcp -n 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 1920x1080x24" rosrun rviz rviz -d $(rospack find raspicat_waypoint_navigation)/config/rviz/raspicat_waypoint_navigation.rviz --fullscreen &
@@ -16,9 +17,11 @@ export DISPLAY=:44
 roslaunch raspicat_waypoint_navigation ci_test.launch \
   mcl:=amcl waypoint_yaml_file:=$(rospack find raspicat_waypoint_navigation)/test/waypoint.yaml \
   map_name:=tsudanuma_2_19 &
+sleep 50
 
 # Record
 ffmpeg -nostdin -draw_mouse 0 -f x11grab -video_size 1300x1000 -i :44 -codec:v libx264 -r 1 /tmp/report/video.mp4 &
+sleep 10
 
 # Execute start operation
 rosservice call --wait /way_nav_start &
